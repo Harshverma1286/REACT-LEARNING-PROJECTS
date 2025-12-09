d2d80f1ea087bbffebe3653fb9f1621f66f852b3
Harsh Verma <hv0626266@gmail.com>
2025-12-09 15:18:53 +0530

    feat: initialize 03project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Created Vite configuration file for project setup.
    - Implemented main application structure in App.jsx with password generation functionality.
    - Added styles using Tailwind CSS in App.css and index.css.
    - Included SVG assets for branding (Vite and React logos).
    - Set up entry point in main.jsx to render the application.

---
+++ 03project/src/App.jsx
@@ -0,0 +1,80 @@
+import { useCallback, useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+  let [length,setlength] = useState(8);
+
+  let [password,setpassword] = useState("");
+
+  let [numallowed,setnumallowed] = useState(false);
+
+  let[charallowed,setcharallowed] = useState(false);
+
+  const passwordgenerator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if(numallowed){
+      str+="123456789";
+    }
+    if(charallowed){
+      str+="!@$#%^&*(){}?";
+    }
+
+    for(let i=1;i<=length;i++){
+      let randomidx = Math.floor(Math.random()*str.length+1);
+
+      pass+=str[randomidx];
+    }
+
+    setpassword(pass);
+  },[length,numallowed,charallowed,setpassword]);
+
+  useEffect(()=>{
+    passwordgenerator();
+  },[passwordgenerator]);
+
+  return (
+    <>
+     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input type="text"
+        value={password}
+        className="outline-none w-full py-1 px-3"
+        placeholder='password'
+        readOnly />
+        <button
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <input type="range" 
+        min={6}
+        max={100}
+        value={length}
+        className='cursor-pointer'
+        onChange={(e)=>{setlength(e.target.value)}}
+         />
+         <label>length:{length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+        <input type="checkbox"
+        defaultChecked={numallowed}
+        id='numberInput'
+        onChange={()=> {setnumallowed((prev)=>!prev)}} />
+        <label htmlFor='numberInput'>Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+        <input type="checkbox"
+        defaultChecked={charallowed}
+        id="charinput"
+        onChange={()=>{setcharallowed((prev)=>!prev)}} />
+        <label htmlFor="charinput">characters</label>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
