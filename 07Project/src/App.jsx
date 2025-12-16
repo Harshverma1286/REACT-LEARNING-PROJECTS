874ea003416779380c5f26b7d53e310fb8d688bf
Harsh Verma <hv0626266@gmail.com>
2025-12-16 16:51:33 +0530

    feat: integrate Tailwind CSS and implement theme toggle functionality
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Refactored App component to include theme context and toggle button.
    - Created ThemeBtn component for switching between light and dark themes.
    - Added Card component to display product information with ratings and an "Add to cart" button.
    - Updated App.css to remove old styles and utilize Tailwind CSS classes.
    - Created theme context to manage theme state across the application.
    - Updated index.css to import Tailwind CSS and set up custom dark mode variants.

--- 07Project/src/App.jsx
+++ 07Project/src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import ThemeBtn from './components/Themebtn'
+import Card from './components/Card'
+import { ThemeProvider } from './context/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [thememode,setthememode] = useState("light");
+
+  const darktheme = ()=>{
+    setthememode("dark");
+  }
+
+  const lighttheme = ()=>{
+    setthememode("light");
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light","dark");
+
+    document.querySelector('html').classList.add(thememode);
+  },[thememode]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ThemeProvider value={{thememode,darktheme,lighttheme}}>
+            <div className="flex flex-wrap min-h-screen items-center">
+                      <div className="w-full">
+                            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                                {<ThemeBtn/>}
+                            </div>
+                            <div className="w-full max-w-sm mx-auto">
+                              {<Card/>}
+                            </div>
+                        </div>
+          </div>
+    </ThemeProvider>
     </>
   )
 }
