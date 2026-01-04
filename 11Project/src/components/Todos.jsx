43be9e07425f36e0e1bdba11f6179f13164151ab
Harsh Verma <hv0626266@gmail.com>
2026-01-04 16:16:14 +0530

    feat: add edit and save functionality to Todos component

--- 11Project/src/components/Todos.jsx
+++ 11Project/src/components/Todos.jsx
@@ -1,15 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 
 import { useDispatch , useSelector } from "react-redux";
 
-import { removetodo } from "../Slices/TodoSlice";
+import { removetodo,updatetodo } from "../Slices/TodoSlice";
 
 function Todos(){
 
     const dispatch = useDispatch();
 
     const todos = useSelector((state)=>state.todo.todos);
+    const [todoid,settodoid] = useState(null);
+    const [todotext,settodotext] = useState('');
 
 
     return(
@@ -20,6 +22,38 @@ function Todos(){
             key={todo.id}
             className="flex items-center justify-between bg-white p-3 mb-2 rounded-lg shadow-sm border border-gray-200 hover:shadow-md transition-shadow"
             >
+            {todoid==todo.id ? (
+                <input type="text" value={todotext} onChange={(e)=> settodotext(e.target.value)} />
+            ):
+            <div>
+                {todo.text}
+            </div>
+            }
+            <div>
+                {todoid==todo.id ? (
+                    <button
+                    onClick={()=> {
+                        dispatch(updatetodo({
+                        id:todoid,
+                        text:todotext,
+                    }))
+                    settodoid(null);
+                        }
+                    }
+                    >
+                        save
+                    </button>
+                ) :
+                (
+                    <button
+                    onClick={()=>{
+                        settodoid(todo.id);
+                        settodotext(todo.text);
+                    }}>
+                        edit
+                    </button>
+                )}
+            </div>
             <span className="text-gray-700">{todo.text}</span>
             <button 
                 onClick={() => dispatch(removetodo(todo.id))}
